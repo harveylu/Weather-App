88ee95e18d2168c2b3064ace689ba5800e3e5433
harvey <minglei.lu@sjsu.edu>
2019-04-10 00:10:13 -0700

    Delete cookie button function fixed

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var cityController = require("../controllers/cityController.js");
-const cookieLife = 30 * 24 * 60 * 60 * 1000; // one month
 
 router.get('/', function(req, res, next){
     cityController.cityDataRetrieval(req.query.city, req.cookies.city, function (error, result){
         if(!error){
             if(result.cookie){
-                res.cookie('city', result.city, { maxAge: cookieLife, httpOnly: true });
+                res.cookie('city', result.city);
             }
             res.render('index', { title: result.title , record: result.record, cities: result.citiesList});
         } else {
